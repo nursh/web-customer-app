5cd1843cca1fe27d0cacca84a140af73afb0a429
nursh <nsheikhmohammed@yahoo.com>
2018-09-12 17:40:50 -0400

    Created CustomerService layer to handle Transaction between DAO and application

--- src/main/java/com/nursh/controller/HomeController.java
+++ src/main/java/com/nursh/controller/HomeController.java
@@ -1,12 +1,12 @@
 package com.nursh.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
 
-    @RequestMapping("/")
+    @GetMapping("/")
     public String redirectToCustomerList() {
         return "redirect:/customer/list";
     }
---
+++ src/main/java/com/nursh/service/CustomerService.java
@@ -0,0 +1,10 @@
+package com.nursh.service;
+
+import com.nursh.entity.Customer;
+
+import java.util.List;
+
+public interface CustomerService {
+
+    public List<Customer> getCustomers();
+}
